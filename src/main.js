84888fbec248f414476971f5aa52b3b3c18bc180
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2018-12-31 19:45:40 +0530

    initialisation functions

--- src/main.js
+++ src/main.js
@@ -1,7 +1,41 @@
-var UIcontroller = () => {
+var UIcontroller = (function() {
 
-};
+    var DOMStrings = {
+        keycode: ".keycode"
+    }; 
 
-var controller = () => {
+    return {
 
-};
+        getDOMStrings: () => {
+            return DOMStrings;
+        }
+    };
+})();
+
+var controller = (function(UICtrl) {
+
+    var setUpEventListeners = () => {
+        var DOM;
+        DOM = UICtrl.getDOMStrings();
+
+        // https://developer.mozilla.org/en-US/docs/Web/Events/keypress 
+        document.addEventListener("keypress", (e) => {
+            console.log(e);
+            // console.log(e.key);
+            // console.log(e.keyCode);
+            // console.log(e.code);
+            // console.log(e.charCode);
+        });
+    };
+
+    return {
+
+        init: () => {
+            setUpEventListeners();
+        }
+    };
+
+
+})(UIcontroller);
+
+controller.init();
